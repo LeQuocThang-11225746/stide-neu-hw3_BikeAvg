fcd9833b5869ea00cd14ce538d4bd05f8fa32db7
LeQuocThang-11225746 <128773368+LeQuocThang-11225746@users.noreply.github.com>
2023-04-07 11:32:46 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
-/******************************************************************************
-Copy đoạn code vào file này
-**Lưu ý không đổi tên file hoặc định dạng file
-**Nộp thành công sẽ có tích xanh ở ngoài repository
-*******************************************************************************/
 #include <stdio.h>
-
-int main()
-{
-    printf("Hello World");
-
+    int main()
+{ 
+   int x;
+   float y;
+   printf ("Total distance travelled :");
+   scanf ("%d",&x);
+   printf("Total spent fuel :");
+   scanf ("%f",&y);
+   printf("Bike’s average consumption %f", y/x*100
+   );
+   
     return 0;
 }
